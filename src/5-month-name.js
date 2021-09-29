b81332e7774cf7c88db7261f95edaec8cb325a7f
molbimien <43146046+molbimien@users.noreply.github.com>
2021-09-29 11:05:40 +0200

    installed npm and added code for unit tests 1-6

--- src/5-month-name.js
+++ src/5-month-name.js
@@ -3,6 +3,20 @@ const months = [
   'July', 'August', 'September', 'October', 'November', 'December'
 ]
 
+// export const monthName = (monthNumber) => {
+//   if (monthNumber > 12) {
+//     return null
+//   } else if (monthNumber < 1) {
+//     return null
+//   } else {
+//     return months[monthNumber - 1]
+//   }
+// }
+
 export const monthName = (monthNumber) => {
-  return months[monthNumber]
+  if (monthNumber >= 1 && monthNumber <= 12) {
+    return months[monthNumber - 1]
+  } else {
+    return null
+  }
 }
